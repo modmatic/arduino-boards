c13401486fbb405b61e61d75129f0db0b7d22837
Andrew Meyer <ameyer@modmatic.io>
2020-01-13 06:21:25 -0600

    wip2

--- samd/variants/dotMG_Cart_SAMD21E/variant.h
+++ samd/variants/dotMG_Cart_SAMD21E/variant.h
@@ -60,7 +60,7 @@ extern "C" unsigned int PINCOUNT_fn();
 #define NUM_DIGITAL_PINS     (20u)
 #define NUM_ANALOG_INPUTS    (6u)
 #define NUM_ANALOG_OUTPUTS   (1u)
-#define analogInputToDigitalPin(p)  ((p < 4u) ? (p) : -1) // TODO!
+#define analogInputToDigitalPin(p)  ((p <= 8u) ? (p) : -1)
 
 #define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
 #define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P].ulPin )
@@ -78,22 +78,29 @@ extern "C" unsigned int PINCOUNT_fn();
  */
 // #define digitalPinToTimer(P)
 
-// LEDs
-// TODO
-#define PIN_LED_12           (12u)
-#define PIN_LED              PIN_LED_12
-#define LED_BUILTIN          PIN_LED_12
+/*
+ * LEDs
+ *
+ * This board does not contain LEDs, but these are defined as a
+ * matter of convention, and for using the board outside of dotMG.
+ */
+#define PIN_LED_13           (13u)
+#define PIN_LED              PIN_LED_13
+#define LED_BUILTIN          PIN_LED_13
 
 /*
  * Analog pins
  */
-#define PIN_A0               (14ul)
-#define PIN_A1               (15ul)
-#define PIN_A2               (16ul)
-#define PIN_A3               (17ul)
-#define PIN_A4               (18ul)
-#define PIN_A5               (19ul)
-#define PIN_DAC0             (14ul)
+#define PIN_A0               (0ul)
+#define PIN_A1               (1ul)
+#define PIN_A2               (2ul)
+#define PIN_A3               (3ul)
+#define PIN_A4               (4ul)
+#define PIN_A5               (5ul)
+#define PIN_A6               (6ul)
+#define PIN_A7               (7ul)
+#define PIN_A8               (8ul)
+#define PIN_DAC0             PIN_A0
 
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
@@ -101,18 +108,26 @@ static const uint8_t A2  = PIN_A2;
 static const uint8_t A3  = PIN_A3;
 static const uint8_t A4  = PIN_A4;
 static const uint8_t A5  = PIN_A5;
+static const uint8_t A6  = PIN_A6;
+static const uint8_t A7  = PIN_A7;
+static const uint8_t A8  = PIN_A8;
 static const uint8_t DAC0 = PIN_DAC0;
 #define ADC_RESOLUTION		12
 
-// Other pins
-#define PIN_ATN              (38ul)
+/*
+ * ATN represents the slave/chip select pin for the first SPI module,
+ * which is typically the display in this board's case.
+ *
+ * https://forum.arduino.cc/index.php?topic=330088.msg2280038#msg2280038
+ */
+#define PIN_ATN              (16ul)
 static const uint8_t ATN = PIN_ATN;
 
 /*
  * Serial interface
  */
-#define PIN_SERIAL_RX       (0ul)
-#define PIN_SERIAL_TX       (1ul)
+#define PIN_SERIAL_RX       (14ul)
+#define PIN_SERIAL_TX       (15ul)
 #define PAD_SERIAL_TX       (UART_TX_PAD_2)
 #define PAD_SERIAL_RX       (SERCOM_RX_PAD_3)
 
@@ -121,27 +136,27 @@ static const uint8_t ATN = PIN_ATN;
  */
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (22u)
-#define PIN_SPI_MOSI         (23u)
-#define PIN_SPI_SCK          (24u)
-#define PERIPH_SPI           sercom4
-#define PAD_SPI_TX           SPI_PAD_2_SCK_3
-#define PAD_SPI_RX           SERCOM_RX_PAD_0
+#define PIN_SPI_MISO         (11u)
+#define PIN_SPI_MOSI         (10u)
+#define PIN_SPI_SCK          (9u)
+#define PERIPH_SPI           sercom1
+#define PAD_SPI_TX           SPI_PAD_0_SCK_1
+#define PAD_SPI_RX           SERCOM_RX_PAD_3
 
-static const uint8_t SS	  = PIN_A2 ;	// SERCOM4 last PAD is present on A2 but HW SS isn't used. Set here only for reference.
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
+static const uint8_t SS	  = PIN_A0;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
 
 /*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (20u)
-#define PIN_WIRE_SCL         (21u)
-#define PERIPH_WIRE          sercom3
-#define WIRE_IT_HANDLER      SERCOM3_Handler
+#define PIN_WIRE_SDA         (1u)
+#define PIN_WIRE_SCL         (2u)
+#define PERIPH_WIRE          sercom0
+#define WIRE_IT_HANDLER      SERCOM0_Handler
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
@@ -149,19 +164,19 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * USB
  */
-#define PIN_USB_DM          (28ul)
-#define PIN_USB_DP          (29ul)
+#define PIN_USB_DM          (19ul)
+#define PIN_USB_DP          (20ul)
 
 /*
  * I2S Interfaces
  */
-#define I2S_INTERFACES_COUNT 1
+#define I2S_INTERFACES_COUNT 0
 
-#define I2S_DEVICE          0
-#define I2S_CLOCK_GENERATOR 3
-#define PIN_I2S_SD          (9u)
-#define PIN_I2S_SCK         (1u)
-#define PIN_I2S_FS          (0u)
+// #define I2S_DEVICE          0
+// #define I2S_CLOCK_GENERATOR 3
+// #define PIN_I2S_SD          (9u)
+// #define PIN_I2S_SCK         (1u)
+// #define PIN_I2S_FS          (0u)
 
 #ifdef __cplusplus
 }
